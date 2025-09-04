964f47de60f8bd5fa4fa2ecba3d535e93b5e763c
Shivananda Sai <shivanandasai.38@gmail.com>
2025-09-04 21:06:07 +0530

    feat: initial database setup with user, session, account, and verification tables
    - Created migration file for initial database schema
    - Defined User, Session, Account, and Verification models in Prisma schema
    - Added unique constraints and foreign key relationships
    - Configured SQLite as the database provider

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,90 @@
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <section className="flex min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+      <form
+        action=""
+        className="bg-muted m-auto h-fit w-full max-w-sm overflow-hidden rounded-[calc(var(--radius)+.125rem)] border shadow-md shadow-zinc-950/5 dark:[--color-muted:var(--color-zinc-900)]"
+      >
+        <div className="bg-card -m-px rounded-[calc(var(--radius)+.125rem)] border p-8 pb-6">
+          <div className="text-center">
+            <Link href="/" aria-label="go home" className="mx-auto block w-fit">
+              <Icons.logo height={24} width={24} />
+            </Link>
+            <h1 className="mb-1 mt-4 text-xl font-semibold">
+              Sign In to Better Auth Starter
+            </h1>
+            <p className="text-sm">Welcome back! Sign in to continue</p>
+          </div>
+
+          <div className="mt-6 space-y-6">
+            <div className="space-y-2">
+              <Label htmlFor="email" className="block text-sm">
+                Username
+              </Label>
+              <Input type="email" required name="email" id="email" />
+            </div>
+
+            <div className="space-y-0.5">
+              <div className="flex items-center justify-between">
+                <Label htmlFor="pwd" className="text-sm">
+                  Password
+                </Label>
+                <Button asChild variant="link" size="sm">
+                  <Link
+                    href="#"
+                    className="link intent-info variant-ghost text-sm"
+                  >
+                    Forgot your Password ?
+                  </Link>
+                </Button>
+              </div>
+              <Input
+                type="password"
+                required
+                name="pwd"
+                id="pwd"
+                className="input sz-md variant-mixed"
+              />
+            </div>
+
+            <Button className="w-full">Sign In</Button>
+          </div>
+
+          <div className="my-6 grid grid-cols-[1fr_auto_1fr] items-center gap-3">
+            <hr className="border-dashed" />
+            <span className="text-muted-foreground text-xs">
+              Or continue With
+            </span>
+            <hr className="border-dashed" />
+          </div>
+
+          <div className="grid grid-cols-2 gap-3">
+            <Button type="button" variant="outline">
+              <Icons.google />
+              <span>Google</span>
+            </Button>
+            <Button type="button" variant="outline">
+              <Icons.gitHub />
+              <span>GitHub</span>
+            </Button>
+          </div>
+        </div>
+
+        <div className="p-3">
+          <p className="text-accent-foreground text-center text-sm">
+            Don't have an account ?
+            <Button asChild variant="link" className="px-2">
+              <Link href="#">Create account</Link>
+            </Button>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
+}
---
+++ lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "sqlite", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 8,
+    maxPasswordLength: 128,
+    autoSignIn: true,
+  },
+  account: {
+    accountLinking: {
+      enabled: true,
+    },
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
